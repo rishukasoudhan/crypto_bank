1801128e2eb1bf0fb1e13f1687bb3c58e7dfd07b
Shivam <shivamkasoudhan7@gmail.com>
2023-03-23 01:35:57 +0530

    basic-banking-system added

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+module.exports = {
+  solidity: 
+  {version :"0.8.0"
+},
+paths:{
+  artifacts:".client/src/artifacts",
+}
+};
